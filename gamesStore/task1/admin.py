20231021d093186c62a88eb7eb05c3ec0590ee4b
Mish0ka0 <sega.vasilev.2014@mail.ru>
2025-01-29 13:12:26 +0300

    работа с бд

--- gamesStore/task1/admin.py
+++ gamesStore/task1/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from task1.models import *
 
 # Register your models here.
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):              # l: test p: 1234
+    list_display = ('title', 'cost', 'size')
+    list_filter = ('size', 'cost')
+    search_fields = ('title',)
+    list_per_page = 20
+
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')
+    list_filter = ('balance', 'age')
+    search_fields = ('name',)
+    list_per_page = 30
+    readonly_fields = ('balance',)
+
